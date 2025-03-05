ca98accea7573482bcd0933980000e39905e8fe8
Andry Famantanantsoa <handriamirado@gmail.com>
2025-03-05 23:00:32 +0300

    [feat-user-login] debug imports module dependencies

--- src/users/user.schema.ts
+++ src/users/user.schema.ts
@@ -1,39 +1,45 @@
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
 import { Document } from 'mongoose';
-import { IsEmail, IsNotEmpty, isString, IsString, MinLength } from 'class-validator';
+import {
+  IsEmail,
+  IsNotEmpty,
+  isString,
+  IsString,
+  MinLength,
+} from 'class-validator';
 
 export type UserDocument = User & Document;
 @Schema()
 export class User extends Document {
-    @Prop({ required: false })
-    @IsString()
-    @IsNotEmpty()
-    name: string;
-    
-    @Prop({ required: false })
-    @IsString()
-    @IsNotEmpty()
-    firstname: string;
+  @Prop({ required: false })
+  @IsString()
+  @IsNotEmpty()
+  name: string;
 
-    @Prop({ required: true, unique: true })
-    @IsEmail()
-    email: string;
+  @Prop({ required: false })
+  @IsString()
+  @IsNotEmpty()
+  firstname: string;
 
-    @Prop({ required: true })
-    @IsString()
-    @MinLength(8)
-    password: string;
+  @Prop({ required: true, unique: true })
+  @IsEmail()
+  email: string;
 
-    @Prop({ required: false, default: false })
-    isResettingPassword: boolean;
+  @Prop({ required: true })
+  @IsString()
+  @MinLength(8)
+  password: string;
 
-    @Prop({ required: false, unique: true })
-    @IsString()
-    resetPasswordToken?: string;
+  @Prop({ required: false, default: false })
+  isResettingPassword: boolean;
 
-    @Prop({ required: false, unique: true })
-    @IsString()
-    avatarFileKey?: string;
+  @Prop({ required: false, unique: true })
+  @IsString()
+  resetPasswordToken?: string;
+
+  @Prop({ required: false, unique: true })
+  @IsString()
+  avatarFileKey?: string;
 }
 
 export const UserSchema = SchemaFactory.createForClass(User);
--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,12 +1,15 @@
 import { Module } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { MongooseModule } from '@nestjs/mongoose';
-import { UserSchema } from './user.schema';
+import { User, UserSchema } from './user.schema';
 import { UserController } from './users.controller';
+import { AwsS3Service } from 'src/aws/aws-s3.service';
 
 @Module({
-  imports: [MongooseModule.forFeature([{ name: 'User', schema: UserSchema }])],
-  providers: [UsersService],
+  imports: [
+    MongooseModule.forFeature([{ name: User.name, schema: UserSchema }]),
+  ],
+  providers: [UsersService, AwsS3Service],
   controllers: [UserController],
   exports: [UsersService],
 })
