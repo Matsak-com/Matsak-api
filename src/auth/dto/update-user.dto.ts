e2160d4fe50946d05b1334e25a120c658524198e
kaloina <rakotoharilalakaloinaalicia@gmail.com>
2025-03-27 10:19:11 +0300

    [dev] page profil : mis a jour profil admin

---
+++ src/auth/dto/update-user.dto.ts
@@ -0,0 +1,15 @@
+import { IsOptional, IsString, IsEmail } from 'class-validator';
+
+export class UpdateUserDto {
+  @IsOptional()
+  @IsString()
+  name?: string;
+
+  @IsOptional()
+  @IsString()
+  firstname?: string;
+
+  @IsOptional()
+  @IsEmail()
+  email?: string;
+}
--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -3,10 +3,14 @@ import {
   Get,
   Param,
   Post,
+  Body,
+  Patch,
   Req,
   UploadedFile,
   UseGuards,
   UseInterceptors,
+  HttpException,
+  HttpStatus,
 } from '@nestjs/common';
 import { FileInterceptor } from '@nestjs/platform-express';
 // import { JwtAuthGuard } from 'src/auth/jwt-auth.guard';
@@ -14,6 +18,9 @@ import { JwtAuthGuard } from '../auth/jwt-auth.guard';
 import { RequestWithUser } from 'src/auth/jwt/jwt.strategy';
 import { fileSchema } from './utils/file-utils';
 import { UsersService } from './users.service';
+import { CreateUserDto } from '../auth/dto/create-user.dto';
+import { UpdateUserDto } from 'src/auth/dto/update-user.dto';
+import { UpdatePasswordDto } from 'src/auth/dto/update-password.dto';
 
 @Controller('users')
 export class UserController {
@@ -32,17 +39,36 @@ export class UserController {
     });
   }
 
+  // MISE A JOUR AVATAR USER 
+
+  // @UseGuards(JwtAuthGuard)
+  // @UseInterceptors(FileInterceptor('avatar'))
+  // @Post()
+  // async updateUser(
+  //   @Req() requestWithUser: RequestWithUser,
+  //   @UploadedFile() file: Express.Multer.File,
+  // ) {
+  //   const submittedFile = fileSchema.parse(file);
+  //   return this.usersService.updateUser({
+  //     userId: requestWithUser.user.userId,
+  //     submittedFile,
+  //   });
+  // }
+
+
   @UseGuards(JwtAuthGuard)
-  @UseInterceptors(FileInterceptor('avatar'))
-  @Post()
+  @Patch('/:userId')
   async updateUser(
-    @Req() requestWithUser: RequestWithUser,
-    @UploadedFile() file: Express.Multer.File,
+    @Param('userId') userId: string,
+    @Body() updateUserDto: UpdateUserDto & UpdatePasswordDto,
   ) {
-    const submittedFile = fileSchema.parse(file);
-    return this.usersService.updateUser({
-      userId: requestWithUser.user.userId,
-      submittedFile,
-    });
+    try {
+      return await this.usersService.updateUser(userId, updateUserDto);
+    } catch (error) {
+      throw new HttpException(error.message, HttpStatus.BAD_REQUEST);
+    }
   }
+
+
+  
 }
