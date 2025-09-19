9e587455306c491fee740dc4a94a3575363e2be3
Andry Famantanantsoa <handriamirado@gmail.com>
2025-09-19 09:23:15 +0300

    fix: convert teamId to ObjectId in findByTeamAndType method for proper filtering

--- src/team-param/team-param.repository.ts
+++ src/team-param/team-param.repository.ts
@@ -3,6 +3,7 @@ import { InjectModel } from '@nestjs/mongoose';
 import { Model, FilterQuery } from 'mongoose';
 import { BaseRepository } from '../common/base.repository';
 import { TeamParam, TeamParamDocument } from './team-param.schema';
+import { Types } from 'mongoose';
 
 @Injectable()
 export class TeamParamRepository extends BaseRepository<TeamParamDocument> {
@@ -33,7 +34,7 @@ export class TeamParamRepository extends BaseRepository<TeamParamDocument> {
   }): Promise<TeamParamDocument[]> {
     return this.findAll({
       filter: {
-        team: teamId,
+        team: new Types.ObjectId(teamId),
         paramType,
       } as FilterQuery<TeamParamDocument>,
       options: { populate: [{ path: 'team' }] },
