0cef71207efcad5e748427641dd2714b6c7a3bf6
Klicia4 <rakotoharilalakaloinaalicia@gmail.com>
2025-12-30 16:43:02 +0300

    [fix] fix/cart : fix check

--- src/auth/optional-jwt.guard.ts
+++ src/auth/optional-jwt.guard.ts
@@ -4,7 +4,7 @@ import { AuthGuard } from '@nestjs/passport';
 @Injectable()
 export class OptionalJwtAuthGuard extends AuthGuard('jwt') {
   // ✅ Ne lance pas d'erreur si pas de token, retourne juste null
-  handleRequest(err: any, user: any, info: any, context: ExecutionContext) {
+  handleRequest(err: any, user: any) {
     // Si l'utilisateur existe (token valide), on le retourne
     // Sinon, on retourne null (pas d'erreur)
     return user || null;
